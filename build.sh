ea087357edcbe46921688c7dd8c8111bdfbc9f9c
brtgmaden <bartug.maden@orioninc.com>
2022-08-30 20:02:04 +0300

    test

---
+++ build.sh
@@ -0,0 +1,202 @@
+#!/bin/sh
+
+## WebRTC library build script
+## Created by Stasel
+## BSD-3 License
+## 
+## Example usage: MACOS=true IOS=true BUILD_VP9=true sh build.sh
+
+# Configs
+DEBUG="${DEBUG:-false}"
+BUILD_VP9="${BUILD_VP9:-false}"
+BRANCH="${BRANCH:-master}"
+IOS="${IOS:-false}"
+MACOS="${MACOS:-false}"
+MAC_CATALYST="${MAC_CATALYST:-false}"
+
+OUTPUT_DIR="./out"
+XCFRAMEWORK_DIR="out/WebRTC.xcframework"
+COMMON_GN_ARGS="is_debug=${DEBUG} rtc_libvpx_build_vp9=${BUILD_VP9} is_component_build=false rtc_include_tests=false rtc_enable_objc_symbol_export=true enable_stripping=true enable_dsyms=false use_lld=true"
+PLISTBUDDY_EXEC="/usr/libexec/PlistBuddy"
+
+build_iOS() {
+    local arch=$1
+    local environment=$2
+    local gen_dir="${OUTPUT_DIR}/ios-${arch}-${environment}"
+    local gen_args="${COMMON_GN_ARGS} target_cpu=\"${arch}\" target_os=\"ios\" target_environment=\"${environment}\" ios_deployment_target=\"12.0\" ios_enable_code_signing=false"
+    gn gen "${gen_dir}" --args="${gen_args}"
+    ninja -C "${gen_dir}" framework_objc || exit 1
+}
+
+build_macOS() {
+    local arch=$1
+    local gen_dir="${OUTPUT_DIR}/macos-${arch}"
+    local gen_args="${COMMON_GN_ARGS} target_cpu=\"${arch}\" target_os=\"mac\""
+    gn gen "${gen_dir}" --args="${gen_args}"
+    ninja -C "${gen_dir}" mac_framework_objc || exit 1
+}
+
+# Catalyst builds are not working properly yet. 
+# See: https://groups.google.com/g/discuss-webrtc/c/VZXS4V4mSY4
+build_catalyst() {
+    local arch=$1
+    local gen_dir="${OUTPUT_DIR}/catalyst-${arch}"
+    local gen_args="${COMMON_GN_ARGS} target_cpu=\"${arch}\" target_environment=\"catalyst\" target_os=\"ios\" ios_deployment_target=\"14.0\" ios_enable_code_signing=false"
+    gn gen "${gen_dir}" --args="${gen_args}"
+    ninja -C "${gen_dir}" framework_objc || exit 1
+}
+
+plist_add_library() {
+    local index=$1
+    local identifier=$2
+    local platform=$3
+    local platform_variant=$4
+    "$PLISTBUDDY_EXEC" -c "Add :AvailableLibraries: dict"  "${INFO_PLIST}"
+    "$PLISTBUDDY_EXEC" -c "Add :AvailableLibraries:${index}:LibraryIdentifier string ${identifier}"  "${INFO_PLIST}"
+    "$PLISTBUDDY_EXEC" -c "Add :AvailableLibraries:${index}:LibraryPath string WebRTC.framework"  "${INFO_PLIST}"
+    "$PLISTBUDDY_EXEC" -c "Add :AvailableLibraries:${index}:SupportedArchitectures array"  "${INFO_PLIST}"
+    "$PLISTBUDDY_EXEC" -c "Add :AvailableLibraries:${index}:SupportedPlatform string ${platform}"  "${INFO_PLIST}"
+    if [ ! -z "$platform_variant" ]; then
+        "$PLISTBUDDY_EXEC" -c "Add :AvailableLibraries:${index}:SupportedPlatformVariant string ${platform_variant}" "${INFO_PLIST}"
+    fi
+}
+
+plist_add_architecture() {
+    local index=$1
+    local arch=$2
+    "$PLISTBUDDY_EXEC" -c "Add :AvailableLibraries:${index}:SupportedArchitectures: string ${arch}"  "${INFO_PLIST}"
+}
+
+# Step 1: Download and install depot tools
+if [ ! -d depot_tools ]; then
+    git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+else
+    cd depot_tools
+    git pull origin main
+    cd ..
+fi
+export PATH=$(pwd)/depot_tools:$PATH
+
+# Step 2 - Download and build WebRTC
+if [ ! -d src ]; then
+    fetch --nohooks webrtc_ios
+fi
+cd src
+git fetch --all
+git checkout $BRANCH
+cd ..
+gclient sync --with_branch_heads --with_tags
+cd src
+
+# Step 2.5 - Apply patches (Temp)
+# sed -i '' 's/"-mllvm",$/# "-mllvm",/g' ./build/config/compiler/BUILD.gn
+# sed -i '' 's/"-instcombine-lower-dbg-declare=0",$/# "-instcombine-lower-dbg-declare=0",/g' ./build/config/compiler/BUILD.gn
+
+# Step 3 - Compile and build all frameworks
+rm -rf $OUTPUT_DIR  
+
+if [ "$IOS" = true ]; then
+    build_iOS "x64" "simulator"
+    build_iOS "arm64" "simulator"
+    build_iOS "arm64" "device"
+fi
+
+if [ "$MACOS" = true ]; then
+    build_macOS "x64"
+    build_macOS "arm64"
+fi
+
+if [ "$MAC_CATALYST" = true ]; then
+    build_catalyst "x64"
+    build_catalyst "arm64"
+fi
+
+# Step 4 - Manually create XCFramework.
+# Unfortunately we cannot use xcodebuild `-xcodebuild -create-xcframework` because of an error:
+# "Both ios-arm64-simulator and ios-x86_64-simulator represent two equivalent library definitions."
+# Therefore, we craft the XCFramework manually with multi architecture binaries created by lipo.
+# We also use plistbuddy to create the plist for the XCFramework
+
+INFO_PLIST="${XCFRAMEWORK_DIR}/Info.plist"
+rm -rf "${XCFRAMEWORK_DIR}"
+mkdir "${XCFRAMEWORK_DIR}"
+"$PLISTBUDDY_EXEC" -c "Add :CFBundlePackageType string XFWK"  "${INFO_PLIST}"
+"$PLISTBUDDY_EXEC" -c "Add :XCFrameworkFormatVersion string 1.0"  "${INFO_PLIST}"
+"$PLISTBUDDY_EXEC" -c "Add :AvailableLibraries array" "${INFO_PLIST}"
+
+# Step 5.1 - Add iOS libs to XCFramework
+LIB_COUNT=0
+if [[ "$IOS" = true ]]; then
+
+    IOS_LIB_IDENTIFIER="ios-arm64"
+    IOS_SIM_LIB_IDENTIFIER="ios-x86_64_arm64-simulator"
+
+    mkdir "${XCFRAMEWORK_DIR}/${IOS_LIB_IDENTIFIER}"
+    mkdir "${XCFRAMEWORK_DIR}/${IOS_SIM_LIB_IDENTIFIER}"
+    LIB_IOS_INDEX=0
+    LIB_IOS_SIMULATOR_INDEX=1
+    plist_add_library $LIB_IOS_INDEX $IOS_LIB_IDENTIFIER "ios"
+    plist_add_library $LIB_IOS_SIMULATOR_INDEX $IOS_SIM_LIB_IDENTIFIER "ios" "simulator"
+
+    cp -r out/ios-arm64-device/WebRTC.framework "${XCFRAMEWORK_DIR}/${IOS_LIB_IDENTIFIER}"
+    cp -r out/ios-x64-simulator/WebRTC.framework "${XCFRAMEWORK_DIR}/${IOS_SIM_LIB_IDENTIFIER}"
+
+    LIPO_IOS_FLAGS="out/ios-arm64-device/WebRTC.framework/WebRTC"
+    LIPO_IOS_SIM_FLAGS="out/ios-x64-simulator/WebRTC.framework/WebRTC out/ios-arm64-simulator/WebRTC.framework/WebRTC"
+
+    plist_add_architecture $LIB_IOS_INDEX "arm64"
+    plist_add_architecture $LIB_IOS_SIMULATOR_INDEX "arm64"
+    plist_add_architecture $LIB_IOS_SIMULATOR_INDEX "x86_64"
+
+    lipo -create -output  "${XCFRAMEWORK_DIR}/${IOS_LIB_IDENTIFIER}/WebRTC.framework/WebRTC" ${LIPO_IOS_FLAGS}
+    lipo -create -output "${XCFRAMEWORK_DIR}/${IOS_SIM_LIB_IDENTIFIER}/WebRTC.framework/WebRTC" ${LIPO_IOS_SIM_FLAGS}
+
+    LIB_COUNT=$((LIB_COUNT+2))
+fi
+
+# Step 5.2 - Add macOS libs to XCFramework
+if [ "$MACOS" = true ]; then
+
+    MAC_LIB_IDENTIFIER="macos-x86_64_arm64"
+
+    mkdir "${XCFRAMEWORK_DIR}/${MAC_LIB_IDENTIFIER}"
+    plist_add_library $LIB_COUNT "${MAC_LIB_IDENTIFIER}" "macos"
+    plist_add_architecture $LIB_COUNT "x86_64"
+    plist_add_architecture $LIB_COUNT "arm64"
+
+    cp -RP out/macos-x64/WebRTC.framework "${XCFRAMEWORK_DIR}/${MAC_LIB_IDENTIFIER}"
+    lipo -create -output "${XCFRAMEWORK_DIR}/${MAC_LIB_IDENTIFIER}/WebRTC.framework/Versions/A/WebRTC" out/macos-x64/WebRTC.framework/WebRTC out/macos-arm64/WebRTC.framework/WebRTC
+    LIB_COUNT=$((LIB_COUNT+1))
+fi
+
+# Step 5.3 - macOS catalyst libs to XCFramework
+if [ "$MAC_CATALYST" = true ]; then
+
+    CATALYST_LIB_IDENTIFIER="ios-x86_64_arm64-maccatalyst"
+
+    mkdir "${XCFRAMEWORK_DIR}/${CATALYST_LIB_IDENTIFIER}"
+    plist_add_library $LIB_COUNT "${CATALYST_LIB_IDENTIFIER}" "ios" "maccatalyst"
+    plist_add_architecture $LIB_COUNT "x86_64"
+    plist_add_architecture $LIB_COUNT "arm64"
+
+    cp -RP out/catalyst-x64/WebRTC.framework "${XCFRAMEWORK_DIR}/${CATALYST_LIB_IDENTIFIER}"
+    lipo -create -output "${XCFRAMEWORK_DIR}/${CATALYST_LIB_IDENTIFIER}/WebRTC.framework/WebRTC" out/catalyst-x64/WebRTC.framework/WebRTC out/catalyst-arm64/WebRTC.framework/WebRTC
+    LIB_COUNT=$((LIB_COUNT+1))
+fi
+
+# Step 6 - Add license file to the framework
+cp LICENSE ${XCFRAMEWORK_DIR}
+
+# Step 7 - archive the framework
+cd out
+NOW=$(date -u +"%Y-%m-%dT%H-%M-%S")
+OUTPUT_NAME=WebRTC-$NOW.xcframework.zip
+zip --symlinks -r $OUTPUT_NAME WebRTC.xcframework/
+
+# Step 8 calculate SHA256 checksum
+CHECKSUM=$(shasum -a 256 $OUTPUT_NAME | awk '{ print $1 }')
+COMMIT_HASH=$(git rev-parse HEAD)
+
+echo "{ \"file\": \"${OUTPUT_NAME}\", \"checksum\": \"${CHECKSUM}\", \"commit\": \"${COMMIT_HASH}\", \"branch\": \"${BRANCH}\" }" > metadata.json
+cat metadata.json
+
---
+++ release.py
@@ -0,0 +1,191 @@
+import os
+import json
+import requests
+from datetime import datetime, timedelta
+from dataclasses import dataclass
+import telegram
+
+GITHUB_TOKEN=os.environ.get("GITHUB_TOKEN")
+TELEGRAM_TOKEN=os.environ.get("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID=os.environ.get("TELEGRAM_CHAT_ID")
+
+@dataclass
+class NextReleaseResult:
+    version: int
+    releaseDate: datetime
+    branch: str
+
+@dataclass
+class BuildMetadata:
+    filename: str
+    checksum: str
+    commit: str
+    branch: str
+
+def getNextRelease():
+    # Get current version
+    releases = requests.get("https://api.github.com/repos/stasel/WebRTC/releases").json()
+    latestReleaseVersion = int(releases[0]["name"][1:])
+    latestReleaseDate = datetime.fromisoformat(releases[0]["published_at"].replace("Z", ""))
+    print(f"Latest release: version {latestReleaseVersion}, date: {latestReleaseDate}")
+
+    # Get next version
+    nextReleaseVersion = latestReleaseVersion + 1
+    milestones = requests.get(f"https://chromiumdash.appspot.com/fetch_milestone_schedule?mstone={nextReleaseVersion}").json()
+    nextReleaseDate = datetime.fromisoformat(milestones["mstones"][0]["stable_date"])
+    print(f"Next release:   version {nextReleaseVersion}, date: {nextReleaseDate}")
+
+    # Get next version branch
+    releases = requests.get(f"https://chromiumdash.appspot.com/fetch_milestones?mstone={nextReleaseVersion}").json()
+    nextReleaseBranch = "branch-heads/" + releases[0]["webrtc_branch"]
+
+    return NextReleaseResult(version = nextReleaseVersion, releaseDate = nextReleaseDate, branch = nextReleaseBranch)
+
+def isReleaseAvailable(release):
+    return datetime.today() >= (release.releaseDate + timedelta(days=1))
+
+def buildWebRTC(branch):
+    os.environ["BITCODE"] = "true"
+    os.environ["BUILD_VP9"] = "true"
+    os.environ["BRANCH"] = branch
+    os.environ["IOS"] = "true"
+    os.environ["MACOS"] = "true"
+    os.environ["MAC_CATALYST"] = "true"
+
+    return os.system('sh scripts/build.sh') == 0
+
+def getBuildMetadata(outputDir):
+    with open(f"{outputDir}/metadata.json", 'r') as f:
+        jsonData = json.loads(f.read())
+        return BuildMetadata(filename = jsonData['file'], checksum = jsonData['checksum'], commit = jsonData['commit'], branch = jsonData['branch'])
+
+def createReleaseDraft(release, buildMetadata):
+    body = "Release notes: N/A\n"
+    body += f"WebRTC Branch: [{buildMetadata.branch}](https://chromium.googlesource.com/external/webrtc/+log/{buildMetadata.branch})\n"
+    body += f"WebRTC Commit: `{buildMetadata.commit}`\n"
+    body += f"SHA 256 checksum: `{buildMetadata.checksum}`"
+
+    fields = { 
+        'name': f'M{release.version}',
+        'tag_name': f'{release.version}.0.0',
+        'draft': True,
+        'body': body
+    }
+    headers = {'accept': 'application/vnd.github.v3+json', 'Authorization': f'token {GITHUB_TOKEN}'}
+    return requests.post("https://api.github.com/repos/stasel/WebRTC/releases", json = fields, headers = headers).json()
+
+def uploadReleaseAsset(url, assetLocalPath, assetName):
+    url = url.replace(u'{?name,label}','')
+    fileToUpload = open(assetLocalPath, 'rb')  
+    size = os.stat(assetLocalPath).st_size
+    params = {'name': assetName}
+    headers = {'Authorization': f'token {GITHUB_TOKEN}', 'Content-Length': str(size), 'Content-Type': 'Application/zip'}
+    response = requests.post(url, params = params, data = fileToUpload, headers = headers)
+    success = response.status_code == requests.codes.created
+    if not success:
+        print(response)
+    return success
+
+def createPullRequest(release, head):
+    headers = {'accept': 'application/vnd.github.v3+json', 'Authorization': f'token {GITHUB_TOKEN}'}
+    body = { 
+        'title': f'Release M{release.version}',
+        'head': head,
+        'base': 'latest',
+        'body': 'Created by an automated sotfware 🤖'
+    }
+    response = requests.post("https://api.github.com/repos/stasel/WebRTC/pulls", json = body, headers = headers)
+    success = response.status_code == requests.codes.created
+    if not success:
+        print(response)
+    return success
+
+if __name__ == "__main__":
+    if not GITHUB_TOKEN:
+        print("❌ GITHUB_TOKEN environment variable is not provided")
+        os._exit(os.EX_SOFTWARE)
+
+    # Get next release details
+    print("➡️ Fetching next release...")
+    nextRelease = getNextRelease()
+
+    # Check if it is time for a new reelease
+    if not isReleaseAvailable(nextRelease):
+        print("ℹ️  Next version is not out yet. Skipping build")
+        os._exit(os.EX_OK)
+
+    print(f"✅ {nextRelease}\n")
+    print("✅ New Version is available to build")
+
+    # Build WebRTC Frameworks
+    print("➡️ Building WebRTC Library...")
+    buildSuccess = buildWebRTC(nextRelease.branch)
+    if not buildSuccess:
+        print("❌ WebRTC Build Failed")
+        os._exit(os.EX_SOFTWARE)
+        
+    print("✅ WebRTC build successful\n")
+
+    # Get metadata build file - it has all the information needed about the build
+    outputDir="src/out"
+    buildMetadata = getBuildMetadata(outputDir)
+    print(buildMetadata)
+
+    # Create new release draft
+    print("➡️ Creating new release draft...")
+    githubReleaseDraft = createReleaseDraft(nextRelease ,buildMetadata)
+
+    # Upload asset to github
+    print("➡️ Uploading asset to github...")
+    assetName = f"WebRTC-M{nextRelease.version}.xcframework.zip"
+    assetPath = os.path.join(outputDir, buildMetadata.filename)
+    uploadURL = githubReleaseDraft['upload_url']
+    uploadResult = uploadReleaseAsset(uploadURL, assetPath, assetName)
+
+    if not uploadResult:
+        print("❌ Failed uploading asset to github")
+        os._exit(os.EX_SOFTWARE)
+
+    print(f"✅ Successfully created new draft release in github: {githubReleaseDraft['url']}")
+
+    # Create new branch with code changes
+    print("➡️ Creating local branch...")
+    releaseBranch = f'release-M{nextRelease.version}'
+    os.system(f'git checkout -b {releaseBranch}')
+
+    # Change code
+    print("➡️ Applying code changes...")
+    os.system(f"sed -i '' -E 's/[0-9]+.0.0\/WebRTC-M[0-9]+/{nextRelease.version}.0.0\/WebRTC-M{nextRelease.version}/g' Package.swift WebRTC-lib.podspec")
+    os.system(f"sed -i '' -E 's/checksum: \"[0-9a-f]+\"/checksum: \"{buildMetadata.checksum}\"/g' Package.swift WebRTC-lib.podspec ")
+    os.system(f"sed -i '' -E 's/.upToNextMajor([0-9]+.0.0)/.upToNextMajor({nextRelease.version}.0.0)/g' README.md WebRTC-lib.podspec")
+    cartageFile = open("WebRTC.json", 'r')
+
+    cartageJSON = json.loads(cartageFile.read())
+    cartageJSON[f'{nextRelease.version}.0.0'] = f'https://github.com/stasel/WebRTC/releases/download/{nextRelease.version}.0.0/WebRTC-M{nextRelease.version}.xcframework.zip'
+    cartageFile.close()
+    cartageJSONWrite = open("WebRTC.json", 'w')
+    cartageJSONWrite.write(json.dumps(cartageJSON, indent=4, sort_keys=True))
+    cartageJSONWrite.close()
+
+
+    # Commit and push
+    print("➡️ Commiting and pushing code to remote...")
+    os.system(f'git add .')
+    os.system(f'git commit -m "Updated files for release M{nextRelease.version}"')
+    os.system(f'git push origin {releaseBranch}')
+
+    # Create PR
+    print("➡️ Creating pull request...")
+    prResult = createPullRequest(nextRelease, releaseBranch)
+    if not prResult:
+        print("❌ Failed creating pull request in github")
+        os._exit(os.EX_SOFTWARE)
+
+    # Notify about the new release via Telegram bot
+    if TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
+        print("➡️ Sending Telegram notification...")
+        bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        message = f"New WebRTC Release M{nextRelease.version} is now available.\nCheck the PR here: https://github.com/stasel/WebRTC/pulls"
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+
+    print(f"✅ Done")
